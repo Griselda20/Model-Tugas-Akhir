e32bc5d6a053ef5c55f1f892784101d22a2edc9e
Samuel Sihotang <samsihotang1@gmail.com>
2025-03-11 14:28:44 +0700

    3

---
+++ for_hpc/baru/i_co/2_new_all_300epoch/co.py
@@ -0,0 +1,567 @@
+#region Import
+# @title Import
+from tqdm import tqdm
+from torchvision import datasets, transforms
+from torchvision.transforms import functional as F, ToPILImage
+from torch.utils.data import DataLoader, Dataset, ConcatDataset, random_split, Subset
+from torch.utils.tensorboard import SummaryWriter
+from fvcore.nn import FlopCountAnalysis
+from torchvision.io import read_image, ImageReadMode
+from torchvision.transforms.autoaugment import RandAugment
+
+import math
+import numpy as np
+import pandas as pd
+import time
+import copy
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.models as models
+import torch.optim as optim
+import torchvision.transforms as transforms
+import os, glob
+import zipfile
+from einops import rearrange
+from einops.layers.torch import Rearrange
+import warnings
+warnings.filterwarnings("ignore")
+
+#device 1
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+if torch.cuda.is_available():
+    print(f"Using GPU1: {torch.cuda.get_device_name(torch.cuda.current_device())}")
+else:
+    print("Using CPU")
+
+#endregion
+
+#region Hyperparameter
+# @title Hyperparameter
+
+stochastic_depth_rate = 0.7 #dfault 0.1
+center_crop = True #dfult false
+randaugment_n = 2
+randaugment_m = 15
+# mixup_alpha = 0.8 #ga ada yg make var ini
+# loss_type = "Softmax" #otomatis
+label_smoothing = 0.1
+train_epochs = 300
+train_batch_size = 25
+optimizer_type = "AdamW"
+peak_learning_rate = 1e-3
+min_learning_rate = 5e-5
+warmup_steps = 10000
+lr_decay_schedule = "Cosine"
+weight_decay_rate = 0.05
+gradient_clip = 1.0
+# ema_decay_rate = None #rumit butuh fungsi lain
+#endregion
+
+#region Blocks
+# @title Blocks
+# @title dna
+
+def conv_3x3_bn(inp, oup, image_size, downsample=False):
+    stride = 1 if downsample == False else 2
+    return nn.Sequential(
+        nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
+        nn.BatchNorm2d(oup),
+        nn.GELU()
+    )
+
+
+class PreNorm(nn.Module):
+    def __init__(self, dim, fn, norm):
+        super().__init__()
+        self.norm = norm(dim)
+        self.fn = fn
+
+    def forward(self, x, **kwargs):
+        return self.fn(self.norm(x), **kwargs)
+
+
+class SE(nn.Module):
+    def __init__(self, inp, oup, expansion=0.25):
+        super().__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(oup, int(inp * expansion), bias=False),
+            nn.GELU(),
+            nn.Linear(int(inp * expansion), oup, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y
+
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, hidden_dim, dropout=0.):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(dim, hidden_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, dim),
+            nn.Dropout(dropout)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class MBConv(nn.Module):
+    def __init__(self, inp, oup, image_size, downsample=False, expansion=4):
+        super().__init__()
+        self.downsample = downsample
+        stride = 1 if self.downsample == False else 2
+        hidden_dim = int(inp * expansion)
+
+        if self.downsample:
+            self.pool = nn.MaxPool2d(3, 2, 1)
+            self.proj = nn.Conv2d(inp, oup, 1, 1, 0, bias=False)
+
+        if expansion == 1:
+            self.conv = nn.Sequential(
+                # dw
+                nn.Conv2d(hidden_dim, hidden_dim, 3, stride,
+                          1, groups=hidden_dim, bias=False),
+                nn.BatchNorm2d(hidden_dim),
+                nn.GELU(),
+                # pw-linear
+                nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(oup),
+            )
+        else:
+            self.conv = nn.Sequential(
+                # pw
+                # down-sample in the first conv
+                nn.Conv2d(inp, hidden_dim, 1, stride, 0, bias=False),
+                nn.BatchNorm2d(hidden_dim),
+                nn.GELU(),
+                # dw
+                nn.Conv2d(hidden_dim, hidden_dim, 3, 1, 1,
+                          groups=hidden_dim, bias=False),
+                nn.BatchNorm2d(hidden_dim),
+                nn.GELU(),
+                SE(inp, hidden_dim),
+                # pw-linear
+                nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(oup),
+            )
+
+        self.conv = PreNorm(inp, self.conv, nn.BatchNorm2d)
+
+    def forward(self, x):
+        if self.downsample:
+            return self.proj(self.pool(x)) + self.conv(x)
+        else:
+            return x + self.conv(x)
+
+
+class Attention(nn.Module):
+    def __init__(self, inp, oup, image_size, heads=8, dim_head=32, dropout=0.):
+        super().__init__()
+        inner_dim = dim_head * heads
+        project_out = not (heads == 1 and dim_head == inp)
+
+        self.ih, self.iw = image_size
+
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+
+        # parameter table of relative position bias
+        self.relative_bias_table = nn.Parameter(
+            torch.zeros((2 * self.ih - 1) * (2 * self.iw - 1), heads))
+
+        coords = torch.meshgrid((torch.arange(self.ih), torch.arange(self.iw)))
+        coords = torch.flatten(torch.stack(coords), 1)
+        relative_coords = coords[:, :, None] - coords[:, None, :]
+
+        relative_coords[0] += self.ih - 1
+        relative_coords[1] += self.iw - 1
+        relative_coords[0] *= 2 * self.iw - 1
+        relative_coords = rearrange(relative_coords, 'c h w -> h w c')
+        relative_index = relative_coords.sum(-1).flatten().unsqueeze(1)
+        self.register_buffer("relative_index", relative_index)
+
+        self.attend = nn.Softmax(dim=-1)
+        self.to_qkv = nn.Linear(inp, inner_dim * 3, bias=False)
+
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, oup),
+            nn.Dropout(dropout)
+        ) if project_out else nn.Identity()
+
+    def forward(self, x):
+        qkv = self.to_qkv(x).chunk(3, dim=-1)
+        q, k, v = map(lambda t: rearrange(
+            t, 'b n (h d) -> b h n d', h=self.heads), qkv)
+
+        dots = torch.matmul(q, k.transpose(-1, -2)) * self.scale
+
+        # Use "gather" for more efficiency on GPUs
+        relative_bias = self.relative_bias_table.gather(
+            0, self.relative_index.repeat(1, self.heads))
+        relative_bias = rearrange(
+            relative_bias, '(h w) c -> 1 c h w', h=self.ih*self.iw, w=self.ih*self.iw)
+        dots = dots + relative_bias
+
+        attn = self.attend(dots)
+        out = torch.matmul(attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        out = self.to_out(out)
+        return out
+
+
+class Transformer(nn.Module):
+    def __init__(self, inp, oup, image_size, heads=8, dim_head=32, downsample=False, dropout=0.):
+        super().__init__()
+        hidden_dim = int(inp * 4)
+
+        self.ih, self.iw = image_size
+        self.downsample = downsample
+
+        if self.downsample:
+            self.pool1 = nn.MaxPool2d(3, 2, 1)
+            self.pool2 = nn.MaxPool2d(3, 2, 1)
+            self.proj = nn.Conv2d(inp, oup, 1, 1, 0, bias=False)
+
+        self.attn = Attention(inp, oup, image_size, heads, dim_head, dropout)
+        self.ff = FeedForward(oup, hidden_dim, dropout)
+
+        self.attn = nn.Sequential(
+            Rearrange('b c ih iw -> b (ih iw) c'),
+            PreNorm(inp, self.attn, nn.LayerNorm),
+            Rearrange('b (ih iw) c -> b c ih iw', ih=self.ih, iw=self.iw)
+        )
+
+        self.ff = nn.Sequential(
+            Rearrange('b c ih iw -> b (ih iw) c'),
+            PreNorm(oup, self.ff, nn.LayerNorm),
+            Rearrange('b (ih iw) c -> b c ih iw', ih=self.ih, iw=self.iw)
+        )
+
+    def forward(self, x):
+        if self.downsample:
+            x = self.proj(self.pool1(x)) + self.attn(self.pool2(x))
+        else:
+            x = x + self.attn(x)
+        x = x + self.ff(x)
+        return x
+
+
+class CoAtNet(nn.Module):
+    def __init__(self, image_size, in_channels, num_blocks, channels, num_classes=1000, block_types=['C', 'C', 'T', 'T']):
+        super().__init__()
+        ih, iw = image_size
+        block = {'C': MBConv, 'T': Transformer}
+
+        self.s0 = self._make_layer(
+            conv_3x3_bn, in_channels, channels[0], num_blocks[0], (ih // 2, iw // 2))
+        self.s1 = self._make_layer(
+            block[block_types[0]], channels[0], channels[1], num_blocks[1], (ih // 4, iw // 4))
+        self.s2 = self._make_layer(
+            block[block_types[1]], channels[1], channels[2], num_blocks[2], (ih // 8, iw // 8))
+        self.s3 = self._make_layer(
+            block[block_types[2]], channels[2], channels[3], num_blocks[3], (ih // 16, iw // 16))
+        self.s4 = self._make_layer(
+            block[block_types[3]], channels[3], channels[4], num_blocks[4], (ih // 32, iw // 32))
+
+        self.pool = nn.AvgPool2d(ih // 32, 1)
+        self.fc = nn.Linear(channels[-1], num_classes, bias=False)
+
+    def forward(self, x):
+        x = self.s0(x)
+        x = self.s1(x)
+        x = self.s2(x)
+        x = self.s3(x)
+        x = self.s4(x)
+
+        x = self.pool(x).view(-1, x.shape[1])
+        x = self.fc(x)
+        return x
+
+    def _make_layer(self, block, inp, oup, depth, image_size):
+        layers = nn.ModuleList([])
+        for i in range(depth):
+            if i == 0:
+                layers.append(block(inp, oup, image_size, downsample=True))
+            else:
+                layers.append(block(oup, oup, image_size))
+        return nn.Sequential(*layers)
+
+#endregion
+
+#region RegisterModel
+# @title register model
+def coatnet_0():
+    num_blocks = [2, 2, 3, 5, 2]            # L
+    channels = [64, 96, 192, 384, 768]      # D
+    return CoAtNet((224, 224), 3, num_blocks, channels, num_classes=1000)
+
+
+def coatnet_1():
+    num_blocks = [2, 2, 6, 14, 2]           # L
+    channels = [64, 96, 192, 384, 768]      # D
+    return CoAtNet((224, 224), 3, num_blocks, channels, num_classes=1000)
+
+
+def coatnet_2():
+    num_blocks = [2, 2, 6, 14, 2]           # L
+    channels = [128, 128, 256, 512, 1026]   # D
+    return CoAtNet((224, 224), 3, num_blocks, channels, num_classes=1000)
+
+
+def coatnet_3():
+    num_blocks = [2, 2, 6, 14, 2]           # L
+    channels = [192, 192, 384, 768, 1536]   # D
+    return CoAtNet((224, 224), 3, num_blocks, channels, num_classes=1000)
+
+
+def coatnet_4():
+    num_blocks = [2, 2, 12, 28, 2]          # L
+    channels = [192, 192, 384, 768, 1536]   # D
+    return CoAtNet((224, 224), 3, num_blocks, channels, num_classes=1000)
+
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
+#endregion
+
+#region Final Dataset
+# @title Final Dataset
+
+batch_size=train_batch_size
+
+class TrainTinyImageNetDataset(Dataset):
+    def __init__(self, transform=None):
+        self.filenames = glob.glob("/home/tasi2425111/resized_imagenet/train/*/*.JPEG")
+        self.transform = transform
+
+        self.class_to_idx = {}
+        class_folders = set([path.split('/')[5] for path in self.filenames])
+        for i, class_name in enumerate(sorted(class_folders)):
+            self.class_to_idx[class_name] = i
+
+    def __len__(self):
+        return len(self.filenames)
+
+    def __getitem__(self, idx):
+        img_path = self.filenames[idx]
+        image = read_image(img_path)
+        if image.shape[0] == 1:
+            image = read_image(img_path, ImageReadMode.RGB)
+
+        class_name = img_path.split('/')[5]
+        label = self.class_to_idx[class_name]
+
+        image = ToPILImage()(image)
+        if self.transform:
+            image = self.transform(image)
+        return image, label
+
+class ValTinyImageNetDataset(Dataset):
+    def __init__(self, transform=None):
+        self.filenames = glob.glob("/home/tasi2425111/resized_imagenet/val/*.JPEG")
+        self.transform = transform
+
+        train_files = glob.glob("/home/tasi2425111/resized_imagenet/train/*/*.JPEG")
+        class_folders = set([path.split('/')[5] for path in train_files])
+        self.class_to_idx = {}
+        for i, class_name in enumerate(sorted(class_folders)):
+            self.class_to_idx[class_name] = i
+
+        self.cls_dic = {}
+        for i, line in enumerate(open('/home/tasi2425111/resized_imagenet/val/val_annotations.txt', 'r')):
+            a = line.split('\t')
+            img, cls_id = a[0], a[1]
+            self.cls_dic[img] = self.class_to_idx[cls_id]
+
+    def __len__(self):
+        return len(self.filenames)
+
+    def __getitem__(self, idx):
+        img_path = self.filenames[idx]
+        image = read_image(img_path)
+        if image.shape[0] == 1:
+            image = read_image(img_path, ImageReadMode.RGB)
+
+        label = self.cls_dic[img_path.split('/')[-1]]
+        image = ToPILImage()(image)
+        if self.transform:
+            image = self.transform(image)
+        return image, label
+
+train_transform = transforms.Compose([
+    RandAugment(num_ops=randaugment_n, magnitude=randaugment_m),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+val_transform = transforms.Compose([
+    # transforms.CenterCrop(64) if center_crop else transforms.Lambda(lambda x: x),
+    # transforms.Resize((64, 64)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+train_dataset = TrainTinyImageNetDataset(transform = train_transform)
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2, pin_memory=True)
+
+val_dataset = ValTinyImageNetDataset(transform=val_transform)
+val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=2, pin_memory=True)
+
+#endregion
+
+#region Variable
+# @title Variable
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import torchvision.models as models
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+import os
+import random
+import numpy as np
+
+# Set Random Seed for Reproducibility
+def set_seed(seed=42):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+set_seed(1234)
+
+# Load Model
+model = coatnet_3()
+model.num_classes = 200
+
+#Lanjutkan training dari pth
+# checkpoint = torch.load("/home/tasi2425111/for_hpc/baru/ti_mf/8_continue_train_newest_dataset_200epochs/best_model_mobile_former.pth", map_location=device)
+# model.load_state_dict(checkpoint["state_dict"])
+
+
+model = model.to(device)
+
+# Loss & Optimizer
+criterion = nn.CrossEntropyLoss(label_smoothing=label_smoothing)
+optimizer = getattr(optim, optimizer_type)(model.parameters(), lr=peak_learning_rate, weight_decay=weight_decay_rate)
+
+scheduler = optim.lr_scheduler.LambdaLR(
+    optimizer,
+    lr_lambda=lambda step: min((step + 1) / warmup_steps, 1.0) * (0.5 * (1 + math.cos(math.pi * step / train_epochs)))
+)
+
+# Early Stopping & Model Saving Variables
+best_val_loss = float("inf")
+patience = 10
+counter = 0
+save_path = "best_model_coatnet.pth"
+
+#endregion
+
+#region Training_Validation
+# @title Training & Validation
+
+def train(model, train_loader, val_loader, criterion, optimizer, scheduler, epochs=train_epochs):
+    global best_val_loss, counter
+
+    for epoch in range(epochs):
+        model.train()
+        train_loss, correct, total = 0, 0, 0
+
+        loop = tqdm(train_loader, desc=f"Epoch [{epoch+1}/{epochs}]", leave=False)
+        for images, labels in loop:
+            images, labels = images.to(device), labels.to(device)
+
+            # Forward pass
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+
+            # Backward pass
+            optimizer.zero_grad()
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), gradient_clip)
+            optimizer.step()
+
+            # Compute metrics
+            train_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += labels.size(0)
+            correct += predicted.eq(labels).sum().item()
+
+            loop.set_postfix(loss=train_loss/(total/batch_size), acc=100.*correct/total)
+
+        # Validation Step
+        model.eval()
+        val_loss, val_correct, val_total = 0, 0, 0
+        with torch.no_grad():
+            for images, labels in val_loader:
+                images, labels = images.to(device), labels.to(device)
+
+                outputs = model(images)
+                loss = criterion(outputs, labels)
+                val_loss += loss.item()
+
+                _, predicted = outputs.max(1)
+                val_total += labels.size(0)
+                val_correct += predicted.eq(labels).sum().item()
+
+        # Loss & Accuracy
+        train_acc = 100. * correct / total
+        val_acc = 100. * val_correct / val_total
+        avg_train_loss = train_loss / len(train_loader)
+        avg_val_loss = val_loss / len(val_loader)
+
+        scheduler.step(avg_val_loss)
+
+        # Epoch Summary
+        print(f"Epoch {epoch+1}/{epochs}: Train Loss={avg_train_loss:.4f}, Train Acc={train_acc:.2f}% | Val Loss={avg_val_loss:.4f}, Val Acc={val_acc:.2f}%")
+
+        # Save Model if Validation Loss Improves
+        if avg_val_loss < best_val_loss:
+            best_val_loss = avg_val_loss
+            counter = 0
+
+            # try:
+            #     checkpoint = torch.load("/home/tasi2425111/for_hpc/baru/ti_mf/8_continue_train_newest_dataset_200epochs/best_model_mobile_former.pth", map_location=device)
+            #     num_epoch = checkpoint['epoch'] + 1
+            # except (FileNotFoundError, KeyError):
+            #     num_epoch = epoch + 1
+
+            # Create new checkpoint with current values
+            checkpoint = {
+                'epoch': epoch + 1,                     # Menyimpan epoch terakhir
+                'state_dict': model.state_dict(),       # Parameter model
+                'optimizer': optimizer.state_dict(),    # State optimizer
+                'metric': val_acc                       # Nilai loss validasi sebagai metric
+            }
+
+            torch.save(checkpoint, save_path)
+            print(f"✅ Model saved at epoch {epoch+1} (val loss improved)")
+        else:
+            counter += 1
+            print(f"⚠️ No improvement in val loss for {counter}/{patience} epochs")
+
+        # Early Stopping Condition
+        if counter >= patience:
+            print("️️⏹️ Early stopping triggered. Training stopped.")
+            break
+
+# Train
+train(model, train_loader, val_loader, criterion, optimizer, scheduler)
+#endregion

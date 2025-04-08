f1994d3d2293acd7faf12863abe6c92615216216
Samuel Sihotang <samsihotang1@gmail.com>
2025-04-08 20:16:37 +0700

    `

--- FINAL/Train_MobileFormer_on_TinyImagenet.py
+++ FINAL/Train_MobileFormer_on_TinyImagenet.py
@@ -2263,9 +2263,9 @@ _logger = logging.getLogger('train')
 class Args:
     ### CUSTOM ###
     # Load this checkpoint as if they were the pretrained weights (with adaptation) (default: None).
-    pretrained_path = None
+    pretrained_path = '/home/tasi2425111/for_hpc/baru/ti_mf/14_compare_result_to13/output/train/20250331-191156-mobile_former_294m-224/checkpoint-119.pth.tar'
     # Resume full model and optimizer state from checkpoint (default: '')
-    resume = ''
+    resume = '/home/tasi2425111/for_hpc/baru/ti_mf/14_compare_result_to13/output/train/20250331-191156-mobile_former_294m-224/checkpoint-119.pth.tar'
     # path to dataset (root dir)
     data_dir = '/home/tasi2425111/restructured-resized-tiny-imagenet-200'  #Disesuaikan dengan kebutuhan
     # number of label classes (Model default if None)
--- FINAL/Train_Modification_on_TinyImagenet.py
+++ FINAL/Train_Modification_on_TinyImagenet.py
@@ -2349,9 +2349,9 @@ _logger = logging.getLogger('train')
 class Args:
     ### CUSTOM ###
     # Load this checkpoint as if they were the pretrained weights (with adaptation) (default: None).
-    pretrained_path = None
+    pretrained_path = '/home/tasi2425111/for_hpc/baru/ti_mod/4_new_train/output/train/20250405-152714-modification_294m-224/checkpoint-75.pth.tar'
     # Resume full model and optimizer state from checkpoint (default: '')
-    resume = ''
+    resume = '/home/tasi2425111/for_hpc/baru/ti_mod/4_new_train/output/train/20250405-152714-modification_294m-224/checkpoint-75.pth.tar'
     # path to dataset (root dir)
     data_dir = '/home/tasi2425111/restructured-resized-tiny-imagenet-200'  #Disesuaikan dengan kebutuhan
     # number of label classes (Model default if None)
@@ -2359,7 +2359,7 @@ class Args:
     # Name of model to train (default: "resnet50")
     model = 'modification_294m'  #Disesuaikan dengan kebutuhan
     # Device (accelerator) to use.
-    device = 'cuda:0'
+    device = 'cuda:1'
     # Input image center crop percent (for validation only)
     crop_pct = None ## Tidak diikutkan karena sudah diresize
     # Use AutoAugment policy. "v0" or "original". (default: None)

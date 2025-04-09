68ef915df536ca6ca95eabfbad58ba37e6a315e8
Samuel Sihotang <samsihotang1@gmail.com>
2025-04-09 21:32:04 +0700

    1

--- for_hpc/ti_mod/4_new_train/mod.py
+++ for_hpc/ti_mod/4_new_train/mod.py
@@ -2349,9 +2349,9 @@ _logger = logging.getLogger('train')
 class Args:
     ### CUSTOM ###
     # Load this checkpoint as if they were the pretrained weights (with adaptation) (default: None).
-    pretrained_path = '/home/tasi2425111/for_hpc/baru/ti_mod/4_new_train/output/train/20250405-152714-modification_294m-224/checkpoint-75.pth.tar'
+    pretrained_path = '/home/tasi2425111/for_hpc/baru/ti_mod/4_new_train/output/train/20250407-085650-modification_294m-224/last.pth.tar'
     # Resume full model and optimizer state from checkpoint (default: '')
-    resume = '/home/tasi2425111/for_hpc/baru/ti_mod/4_new_train/output/train/20250405-152714-modification_294m-224/checkpoint-75.pth.tar'
+    resume = '/home/tasi2425111/for_hpc/baru/ti_mod/4_new_train/output/train/20250407-085650-modification_294m-224/last.pth.tar'
     # path to dataset (root dir)
     data_dir = '/home/tasi2425111/restructured-resized-tiny-imagenet-200'  #Disesuaikan dengan kebutuhan
     # number of label classes (Model default if None)
@@ -2359,7 +2359,7 @@ class Args:
     # Name of model to train (default: "resnet50")
     model = 'modification_294m'  #Disesuaikan dengan kebutuhan
     # Device (accelerator) to use.
-    device = 'cuda:1'
+    device = 'cuda:0'
     # Input image center crop percent (for validation only)
     crop_pct = None ## Tidak diikutkan karena sudah diresize
     # Use AutoAugment policy. "v0" or "original". (default: None)
@@ -2370,7 +2370,7 @@ class Args:
     # Label smoothing (default: 0.1)
     smoothing = 0.1
     # number of epochs to train (default: 300)
-    epochs = 300
+    epochs = 400
     # Input batch size for training (default: 128)
     batch_size = 20
     # Validation batch size override (default: None)
